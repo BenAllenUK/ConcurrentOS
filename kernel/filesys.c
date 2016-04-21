4d299f70886a926cdc37c4006a78d1ebc80c6947
benallen <external@benallen.info>
2016-04-21 17:51:30 +0100

    Creation of inodes and there data

--- kernel/filesys.c
+++ kernel/filesys.c
@@ -1,27 +1,105 @@
 #include "filesys.h"
 supernode_t supnode;
-char buffer[256];
-void toBytes(const void *object, size_t size){
+int next_free_address = 0;
+int next_free_id = 0;
+int write_object(void *object, int size, int addresses[], int fromAddr){
+    char buffer[size];
+    unsigned char* obj_char= (unsigned char*)object;
+    memset( &buffer, 0, sizeof( buffer ) );
     const unsigned char *byte;
+
+    // get bytes
     int pos = 0;
-      for ( byte = object; size--; ++byte ){
-          // printf("%02X", *byte);
-          buffer[pos] = *byte;
-          pos++;
-      }
-      // putchar('\n');
+    int sizeCopy = size;
+    for ( byte = obj_char; sizeCopy--; ++byte ){
+        buffer[pos] = *byte;
+        pos++;
+    }
+
+    // Number of blocks
+    int addressIncrNum;
+    int block_number = (size % 16) ? ((size / 16) + 1) : (size / 16);
+    for ( int i = 0; i < block_number; i++){
+      disk_wr( fromAddr, (uint8_t*)(&(buffer[ i * 16])), WRITE_SIZE );
+      addresses[i] = fromAddr;
+      fromAddr += ADDRESS_INCREMENT;
+      addressIncrNum += 1;
+    }
+    return addressIncrNum;
 }
+void createFile(char name[20], int type, int parentId, void* data, int dataSize){
+
+  // Create new inode
+  inode_t newnode;
+  memset( &newnode, 0, sizeof( inode_t ) );
+
+  // Add in new node properties
+  newnode.id = next_free_id;
+  newnode.type = type;
+  newnode.parentId = parentId;
+  strcpy(newnode.name, name);
+  next_free_id += 1;
+
+  // Write object
+  int addresses[100];
+  int addressNum = write_object(data, dataSize, addresses, next_free_address);
+
+  // Update next free address
+  int lastAddress = addresses[addressNum - 1];
+  next_free_address = lastAddress;
+
+  // Set table data to point newly written data
+  table_t data_table;
+  memset( &data_table, 0, sizeof( table_t ) );
+  for (int i = 0; i < 9; i++){
+      data_table.datablocks[i] = addresses[i];
+  }
+  // TODO: Add table_t pointers
+
+  // Update nodes addresses
+  newnode.data_table = data_table;
+
+  // Write inode
+  int addressNumInode = write_object(&newnode, sizeof(inode_t), addresses, next_free_address);
+
+  // Update next free address
+  lastAddress = addresses[addressNumInode - 1];
+  next_free_address = lastAddress;
+}
+
+
 void files_init(){
+  char data[20] = "hello";
+  createFile("rootfolder", 2, 0, &data, 20);
 
+    // Init root node
+    // inode_t rootnode;
+    // memset( &rootnode, 0, sizeof( inode_t ) );
+    // rootnode.id = 0;
+    // rootnode.parentId = 0;
+    // rootnode.type = 2;
+    // rootnode.name = "root";
+    //
+    // int addresses[100];
+    // int addressNum = write_object(&rootnode, sizeof(inode_t), addresses, 0);
+    //
+    //
+    //
+    //
+    // memset( &supnode, 0, sizeof( supernode_t ) );
+    // supnode.info = 6;
+    // supnode.other = 3;
+    // supnode.a = 1;
+    // supnode.b = 1;
+    // supnode.c = 1;
+    // supnode.d = 1;
+    // int addresses[100];
+    // write_object(&supnode, sizeof(supernode_t), addresses);
+    //
+    // // 0 for p1, 10 for p2
+    // // supernode_t newNode = *(supernode_t*)buffer;
+    // // printf("newnode: %d\n", newNode.info);
 
-    memset( &supnode, 0, sizeof( supernode_t ) );
-    supnode.info = 6;
-    supnode.other = 3;
-
-    unsigned char* ptr= (unsigned char*)&supnode;
-    toBytes(ptr, sizeof(supnode));
-
-    disk_wr( 0, (uint8_t*)(&buffer), WRITE_SIZE );
 }
 
 int main(){
--- kernel/filesys.h
+++ kernel/filesys.h
@@ -9,11 +9,31 @@
 #include "../device/disk.h"
 // #include "disk.h"
 #define WRITE_SIZE 16
+#define ADDRESS_INCREMENT 2
 
 typedef struct {
   int info;
   int other;
+  int d;
+  int a;
+  int b;
+  int c;
 } supernode_t;
+
+typedef struct {
+  int datablocks[9];
+} table_t;
+
+typedef struct {
+  int id; // 4
+  int parentId; // 4
+  int type; // 4
+  char name[20]; // 20
+  table_t data_table; // 4 * 9
+  table_t *single_table; // 4
+  table_t **double_table; // 4
+  table_t ***triple_table; // 4
+} inode_t;
 
 
 void files_init();
